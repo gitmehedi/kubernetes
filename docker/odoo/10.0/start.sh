ac41e2db1018e014d4e2be2fd9e0e7c1dcccb8cb
The Beast <beast@swapon.local>
2019-12-30 01:44:28 +0600

    [FIX] Add '&' in start script.

--- docker/odoo/10.0/start.sh
+++ docker/odoo/10.0/start.sh
@@ -7,7 +7,7 @@ echo -e "===== Create Postgres User    ====="
 su - postgres -c "psql -c \"CREATE ROLE odoo WITH LOGIN PASSWORD 'odoo' SUPERUSER CREATEDB CREATEROLE REPLICATION;\"" 2> /dev/null || true
 
 echo -e "===== Start Odoo Service ====="
-/opt/odoo/server/10.0/odoo-bin -c /etc/odoo/odoo.conf
+/opt/odoo/server/10.0/odoo-bin -c /etc/odoo/odoo.conf &
 
 
 bin/bash
