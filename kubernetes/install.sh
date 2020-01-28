4e5ea498fae3501623dcee24d893bb41feb12467
gitmehedi <git.mehedi@gmail.com>
2020-01-28 08:16:46 +0600

    [MOD] Modify kubernetes installtion scripts.

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 read -p "Node Type Master/Slave (m/s): " SER_TYPE
-read -p "Kubernetes Minikube (mk): " TYPE
+read -p "Kubernetes Hostname: " HOSTNAME
 
 KUBE_VERSION=1.17.0-00
 DOCKER_VERSION=5:18.09.9~3-0~ubuntu-xenial
+IP=`hostname -I | awk '{print $2}'`
 
+echo -e "========== Turn Off SWAP Memory =========="
 sudo swapoff -a
+sudo sed -i 's/\/swapfile/#\/swapfile/g' /etc/fstab
+
+echo -e "========== Change Hostname of Node =========="
+sudo hostnamectl set-hostname $HOSTNAME
+echo -e "$IP $HOSTNAME.com $HOSTNAME" >> /etc/hosts
+echo -e "
+auto enp0s8
+iface enp0s8 inet static
+address $IP" >> /etc/network/interfaces
+
+/etc/init.d/hostname restart
+
 echo -e "========== Remove Older Version of Docker =========="
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
 
@@ -20,6 +34,7 @@ EOF
 
 echo -e "========== Install Docker and Kubernetes packages =========="
 sudo apt-get update -y
+sudo apt-get install -y apt-transport-https
 sudo apt-get install -y docker-ce=$DOCKER_VERSION docker-ce-cli=$DOCKER_VERSION containerd.io
 sudo apt-get install -y conntrack cri-tools ebtables ethtool kubernetes-cni socat
 sudo apt-get install -y kubelet=$KUBE_VERSION kubeadm=$KUBE_VERSION kubectl=$KUBE_VERSION
@@ -30,6 +45,7 @@ sudo apt-mark hold docker-ce kubelet kubeadm kubectl
 echo -e "========== Enable iptables bridge call =========="
 echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
+echo -e "Environment=\"cgroup-driver=systemd/cgroup-driver=cgroupfs\"" >> /etc/systemd/system/kubelet.service.d/10-kubelet.conf
 
 if [ $SER_TYPE == "m" ]
 then
@@ -43,16 +59,6 @@ then
 
   echo -e "========== Install Flannel Networking =========="
   kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/3f7d3e6c24f641e7ff557ebcea1136fdf4b1b6a1/Documentation/kube-flannel.yml
-fi
-
-
-if [ $TYPE == 'mk' ]
-then
-	curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-linux-amd64
-	chmod +x minikube && sudo mv minikube /usr/local/bin/
-	minikube start
-else
-	echo -e "========== No process to execute =========="
 fi
 
 echo -e "========== Version of Kubernetes =========="
