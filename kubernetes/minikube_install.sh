ff8690138316674ab82b8c96456e522e3423591d
gitmehedi <git.mehedi@gmail.com>
2020-02-12 13:52:53 +0600

    [ADD] Add kubernetes installtion script in minikube.

--- kubernetes/minikube_install.sh
+++ kubernetes/minikube_install.sh
@@ -5,16 +5,24 @@ DOCKER_VERSION=5:18.09.9~3-0~ubuntu-xenial
 echo -e "========== Remove Older Version of Docker =========="
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
 
-echo -e "========== Install Docker and Kubernetes packages =========="
+echo -e "========== Install Docker Packages =========="
 sudo apt-get update -y
 sudo apt-get install -y apt-transport-https
-#sudo apt-get install -y docker-ce=$DOCKER_VERSION docker-ce-cli=$DOCKER_VERSION containerd.io
+sudo apt-get install -y virtualbox virtualbox-ext-pack
+
 sudo apt-get install -y docker.io
 sudo setfacl --modify user:${USER}:rw /var/run/docker.sock
-
 mkdir -p /etc/systemd/system/docker.service.d
 systemctl daemon-reload
 systemctl restart docker
+
+echo -e "========== Install Kubernetes Packages =========="
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
 
 echo -e "========== Install minikube in Ubuntu =========="
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
@@ -23,4 +31,4 @@ sudo install minikube /usr/local/bin/
 minikube start --vm-driver=docker
 
 echo -e "========== Install minikube in IOS =========="
-# brew install minikube
+kubectl api-versions
